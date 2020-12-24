b4d4c9397461d0e3bbe5a116e7121fa0e8a42700
Ellen Correa <ellen.inbi@gmail.com>
2020-12-24 14:57:10 -0300

    Module 6 - HTTP Form Posting and Data Access

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm, NgModel } from '@angular/forms';
 import { UserSettings } from '../data/user-settings';
+import { DataService } from '../data/data.service'
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -9,19 +12,47 @@ import { UserSettings } from '../data/user-settings';
 export class UserSettingsFormComponent implements OnInit {
 
   originalUserSettings: UserSettings = {
-    name: 'Milton',
-    emailOffers: true,
-    interfaceStyle: 'dark',
-    subscriptionType: 'Annual',
-    notes: 'here are some notes...'
+    name: null,
+    emailOffers: null,
+    interfaceStyle: null,
+    subscriptionType: null,
+    notes: null
   }
 
   //create a copy from the original form
   userSettings: UserSettings = { ...this.originalUserSettings };
+  postError = false;
+  postErrorMessage = '';
+  subscriptionTypes: Observable<string[]>;
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.subscriptionTypes = this.dataService.getSubscriptionTypes();
   }
 
+  onSubmit(form: NgForm){
+    console.log('in onSubmit: ', form.valid);
+
+    if(form.valid){
+      this.dataService.postUserSettingsForm(this.userSettings).subscribe(
+          result => console.log('success: ', result),
+          error => this.onHttpError(error)
+      );
+    } else{
+      this.postError = true;
+      this.postErrorMessage = "Please fix the above errors"
+    }
+    
+  }
+
+  onBlur(field: NgModel){
+    console.log("in on blur: ", field.valid);
+  }
+
+  onHttpError(errorResponse: any){
+    console.log('error: ', errorResponse);
+    this.postError = true;
+    this.postErrorMessage = errorResponse.error.errorMessage;
+  }
 }
